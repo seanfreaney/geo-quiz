b8f89e045a8fff333d6af5dc37207a3d993c3800
seanfreaney <>
2024-02-08 19:43:23 +0000

    cleaned and formatted list of countries

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,159 +36,159 @@ const countries = [
     population: 1244000
 },
 {
-        country: “Czechia”,
-    capital: “Prague”,
-    language: [“Czech”],
+    country: "Czechia",
+    capital: "Prague",
+    language: ["Czech"],
     population: 10495000
 },
 {
-    country: “Denmark”,
-    capital: “Copenhagen”,
-    language: [“Danish”],
+    country: "Denmark",
+    capital: "Copenhagen",
+    language: ["Danish"],
     population: 5911000
 },
-    {
-        country: “Estonia”,
-    capital: “Tallinn”,
-    language: [“Estonian”],
+{
+    country: "Estonia",
+    capital: "Tallinn",
+    language: ["Estonian"],
     population: 1323000
 },
-    {
-        country: “Finland”,
-    capital: “Helsinki”,
-    language: [“Finnish”],
+{
+    country: "Finland",
+    capital: "Helsinki",
+    language: ["Finnish"],
     population: 5545000
 },
-    {
-        country: “France”,
-    capital: “Paris”,
-    language: [“French”],
+{
+    country: "France",
+    capital: "Paris",
+    language: ["French"],
     population: 64750000
 },
-    {
-        country: “Germany”,
-    capital: “Berlin”,
-    language: [“German”],
+{
+    country: "Germany",
+    capital: "Berlin",
+    language: ["German"],
     population: 83294000
 },
-    {
-        country: “Greece”,
-    capital: “Athens”,
-    language: [“Greek”],
+{
+    country: "Greece",
+    capital: "Athens",
+    language: ["Greek"],
     population: 10341000
 },
-    {
-        country: “Hungary”,
-    capital: “Budapest”,
-    language: [“Hungarian”],
+{
+    country: "Hungary",
+    capital: "Budapest",
+    language: ["Hungarian"],
     population: 10156000
 },
-    {
-        country: “Ireland”,
-    capital: “Dublin”,
-    language: [“Irish”, “English”],
+{
+    country: "Ireland",
+    capital: "Dublin",
+    language: ["Irish", "English"],
     population: 5057000
 },
-    {
-        country: “Italy”,
-    capital: “Rome”,
-    language: [“Italian”],
+{
+    country: "Italy”,
+    capital: "Rome",
+    language: ["Italian"],
     population: 58870000
 },
-    {
-        country: “Latvia”,
-    capital: “Riga”,
-    language: [“Latvian”],
+{
+    country: "Latvia",
+    capital: "Riga",
+    language: ["Latvian"],
     population: 1830000
 },
-    {
-        country: “Lithuania”,
-    capital: “Vilnius”,
-    language: [“Lithuanian”],
+{
+    country: "Lithuania",
+    capital: "Vilnius",
+    language: ["Lithuanian"],
     population: 2718000
 },
-    {
-        country: “Luxembourg”,
-    capital: “Luxembourg”,
-    language: [“Luxembourgish”, “French”, “German”],
+{
+    country: "Luxembourg",
+    capital: "Luxembourg",
+    language: ["Luxembourgish", "French", "German"],
     population: 654000
 },
-    {
-        country: “Malta”,
-    capital: “Valletta”,
-    language: [“Maltese”],
+{
+    country: "Malta",
+    capital: "Valletta",
+    language: ["Maltese"],
     population: 535000 
 },
-    {
-        country: “Netherlands”,
-    capital: “Amsterdam”,
-    language: [“Dutch”],
+{
+    country: "Netherlands",
+    capital: "Amsterdam",
+    language: ["Dutch"],
     population: 17618000
 },
-    {
-        country: “Poland”,
-    capital: “Warsaw”,
-    language: [“Polish”],
+{
+    country: "Poland",
+    capital: "Warsaw",
+    language: ["Polish"],
     population: 41026000
 },
-    {
-        country: “Portugal”,
-    capital: “Lisbon”,
-    language: [“Portuguese”],
+{
+    country: "Portugal",
+    capital: "Lisbon",
+    language: ["Portuguese"],
     population: 10247000
 },
-    {
-        country: “Romania”,
-    capital: “Bucharest”,
-    language: [“Romanian”],
+{
+    country: "Romania",
+    capital: "Bucharest",
+    language: ["Romanian"],
     population: 19890000
 },
-    {
-        country: “Slovakia”,
-    capital: “Bratislava”,
-    language: [“Slovak”],
+{
+    country: "Slovakia",
+    capital: "Bratislava",
+    language: ["Slovak"],
     population: 5795000
 },
-    {
-        country: “Slovenia”,
-    capital: “Ljubljana”,
-    language: [“Slovenian”],
+{
+    country: "Slovenia",
+    capital: "Ljubljana",
+    language: ["Slovenian"],
     population: 2120000
 },
-    {
-        country: “Spain”,
-    capital: “Madrid”,
-    language: [“Spanish”],
+{
+    country: "Spain",
+    capital: "Madrid",
+    language: ["Spanish"],
     population: 47500000
 },
-    {
-        country: “Sweden”,
-    capital: “Stockholm”,
-    language: [“Swedish”],
+{
+    country: "Sweden",
+    capital: "Stockholm",
+    language: ["Swedish"],
     population: 10600000
 },
-    {
-        country: “Russia”,
-    capital: “Moscow”,
-    language: [“Russian”],
+{
+    country: "Russia",
+    capital: "Moscow",
+    language: ["Russian"],
     population: 145000000
 },
-    {
-        country: “England”,
-    capital: “London”,
-    language: [“English”],
+{
+    country: "England",
+    capital: "London",
+    language: ["English"],
     population: 56000000
 },
-    {
-        country: “Scotland”,
-    capital: “Edinburgh”,
-    language: [“English”, “Scots”, “Scottish”, “Scottish Gaelic”],
+{
+    country: "Scotland",
+    capital: "Edinburgh",
+    language: ["English", "Scots", "Scottish", "Scottish Gaelic"],
     population: 5454000
 },
-    {
-        country: “Wales”,
-    capital: “Cardiff”,
-    language: [“Welsh”, “English”],
+{
+    country: "Wales",
+    capital: "Cardiff",
+    language: ["Welsh", "English"],
     population: 3100000
 }
 ];
